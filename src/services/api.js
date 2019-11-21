8a0066344efbb5870b656fcc331e6ba0427dd676
Wali Queiroz <wsantos077@gmail.com>
2019-11-21 10:44:20 -0300

    Base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'http://localhost:8000'});
+const api = axios.create({baseURL: 'https://teste-ffwd.herokuapp.com/'});
 
 export default api;
