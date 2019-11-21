556ca1629706b660f7cac205dc06a116748710be
Wali Queiroz <wsantos077@gmail.com>
2019-11-21 11:05:58 -0300

    Server js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require("express");
+const port = process.env.PORT || 8080;
+const app = express();
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
+app.listen(port);
 
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+console.log("Server started...");
